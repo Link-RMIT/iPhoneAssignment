d2e6f039c907defd0000831a7d4b8fa136bcbda5
Link <s3508932@student.rmit.edu.au>
2016-05-20 16:21:16 +1000

    extension completed

--- Assignment2Extension/TodayViewController.swift
+++ Assignment2Extension/TodayViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 import NotificationCenter
-
+import Alamofire
+import SwiftyJSON
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet weak var quoteText: UILabel!
+    static let API_MEY = "fb20f56600e97a132b2156bbc5f35ae3"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate: date)
+        //quoteText.text = year.description + "-" + month.description + "-" + day.description
+        print("foo")
+
+            //https://api.themoviedb.org/3/movie/upcoming?api_key=fb20f56600e97a132b2156bbc5f35ae3
+        Alamofire.request(.GET,"https://api.themoviedb.org/3/movie/upcoming?api_key=fb20f56600e97a132b2156bbc5f35ae3").responseJSON{ (response)-> Void in
+            
+            dispatch_async(dispatch_get_main_queue()){
+                let upcomming = JSON(response.result.value!)
+                let results = upcomming["results"].arrayValue
+                let theUpcomingMovie = results[Int(arc4random_uniform(UInt32(results.count)))]
+                self.quoteText.text = "Upcoming: \(theUpcomingMovie["original_title"].stringValue)\nReleased on: \(theUpcomingMovie["release_date"])"
+                
+            }
+            
+        }
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,5 +52,8 @@ class TodayViewController: UIViewController, NCWidgetProviding {
 
         completionHandler(NCUpdateResult.NewData)
     }
-    
+    func widgetMarginInsetsForProposedMarginInsets
+        (defaultMarginInsets: UIEdgeInsets) -> (UIEdgeInsets) {
+            return UIEdgeInsetsZero
+    }
 }
